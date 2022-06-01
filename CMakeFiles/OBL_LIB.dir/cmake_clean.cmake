dbe521c7b1591d943c51d99bae27f71e9fa46bbe
ubuntu <ubuntu@localhost.localdomain>
2022-06-01 14:23:08 +0800

    First commit

---
+++ CMakeFiles/OBL_LIB.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OBL_LIB.dir/rise/obl_primitives.cpp.o"
+  "CMakeFiles/OBL_LIB.dir/rise/par_obl_primitives.cpp.o"
+  "libOBL_LIB.pdb"
+  "libOBL_LIB.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OBL_LIB.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
